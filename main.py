1a163123d75c049bbfb46d3de4dd08adec29c1ca
Herick <herickminecraftano@gmail.com>
2025-03-11 17:46:17 -0300

    criar a janela principal

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
 from tkinter import *
 
+def frame_clear(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+def informações():
+    teste = Label(frameinfo, text='janela principal')
+    teste.pack(expand=True)
+
 def botoes():
-    btn1 = Button(framebutton, text='Vendas', font=('arial', 16))
-    btn1.place(relheight=0.05, relwidth=1, relx=0, rely=0.3)
-    btn2 = Button(framebutton, text='Estoque', font=('arial', 16))
-    btn2.place(relheight=0.05, relwidth=1, relx=0, rely=0.36)
+
+    def on_enter(e):
+        e.widget.config(bg='#fafafa')
+    
+    def on_leave(e):
+        e.widget.config(bg='white')
+
+    texto_botões = ['Vendas', 'Estoque', 'Registro de produtos', 'Clientes']
+
+    frame_botões = Frame(framebutton, bg=framebutton.cget('bg'))
+    frame_botões.pack(expand=True, fill=X)
+
+    for texto in texto_botões:
+        btn = Button(frame_botões, text=texto, font=('arial', 16), border=0, bg='white')
+        btn.pack(pady=10, fill=X)
+        btn.bind("<Enter>", on_enter)
+        btn.bind("<Leave>", on_leave)
+    
 
 janela = Tk()
 
 janela.geometry('1920x1040')
 janela.state('zoomed')
-janela.configure(bg='blue')
+janela.configure(bg='#fafafa')
 
 framebutton = Frame(janela, bg='white')
 framebutton.place(relheight=0.99, relwidth=0.15, relx=0.002, rely=0.005)
 
-frameinfo = Frame(janela, bg='green')
+frameinfo = Frame(janela, bg='white')
 frameinfo.place(relheight=0.99, relwidth=0.842, relx=0.155, rely=0.005)
 
 botoes()
+informações()
 
 janela.mainloop()
