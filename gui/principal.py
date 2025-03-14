2749da72125a99089e73cca02a15f8e3e203405e
Herick <herickminecraftano@gmail.com>
2025-03-13 23:11:59 -0300

    terminar a interface inicial

--- gui/principal.py
+++ gui/principal.py
@@ -19,7 +19,26 @@ def janelainicial(frameinfo):
 
     canvas = FigureCanvasTkAgg(fig, master=frameinfo)
     canvas_widget = canvas.get_tk_widget()
-    canvas_widget.place(x=200, y=350)
+    canvas_widget.place(x=150, y=350)
 
     clientes = LabelFrame(frameinfo, bg='white', border=2)
-    clientes.place(relheight=0.3, relwidth=0.5, x=200, y=200)
+    clientes.place(relheight=0.26, relwidth=0.22, x=630, y=370)
+
+    text_clientes = Label(clientes, text='CLIENTES', font=('arial', 20, 'bold'), bg=clientes.cget('bg'))
+    text_clientes.pack(anchor='center')
+
+    clientes_count = StringVar()
+    clientes_count.set(2)
+    clientes_quantidade = Label(clientes, textvariable=clientes_count, font=('arial', 100, 'bold'), bg=clientes.cget('bg'))
+    clientes_quantidade.pack(anchor='center', pady=10)
+
+    produtos = LabelFrame(frameinfo, bg=clientes.cget('bg'), border=2)
+    produtos.place(relheight=0.26, relwidth=0.22, x=1060, y=370)
+
+    text_produtos = Label(produtos, text='PRODUTOS REGISTRADOS', font=('arial', 18, 'bold'), bg=produtos.cget('bg'))
+    text_produtos.pack(anchor='center')
+
+    produtos_count = StringVar()
+    produtos_count.set('40 Mil')
+    produtos_quantidade = Label(produtos, textvariable=produtos_count, font=('arial', 75, 'bold'), bg=produtos.cget('bg'))
+    produtos_quantidade.pack(anchor='center', pady=35)
